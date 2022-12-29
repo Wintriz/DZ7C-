155e99cc594b6f4e3e47aa2a17c8864b8671ed30
Andrey <rf5800@mail.ru>
2022-12-29 15:09:51 +0300

    zad50

--- Zad50/Program.cs
+++ Zad50/Program.cs
@@ -9,3 +9,52 @@
 // 8 4 2 4
 
 // 17 -> такого числа в массиве нет
+
+int[,] array = new int[3, 4];
+Console.WriteLine("Введите число для проверки:");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + "  ");
+    }
+    Console.WriteLine();
+
+}
+int num = int.Parse(Console.ReadLine()!);
+int a = 0;
+int b = 0;
+bool IsNumberInArray = false;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] == num)
+        {
+            IsNumberInArray = true;
+            a = i;
+            b = j;
+            Console.WriteLine($"Число - {num} есть в массиве c индексом: {a};{b}. ");
+        }
+
+    }
+
+}
+if (IsNumberInArray == true)
+{
+    Console.WriteLine();
+}
+if (IsNumberInArray == false)
+{
+    Console.WriteLine("Числа в массиве нет.");
+}
+
+
