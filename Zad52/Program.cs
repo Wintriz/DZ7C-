b0b9fe9a10b0b488aaccd633be1fc0f8fa6890b7
Andrey <rf5800@mail.ru>
2022-12-29 16:18:21 +0300

    zad52

--- Zad52/Program.cs
+++ Zad52/Program.cs
@@ -5,3 +5,32 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = new int [3,4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 11);
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = sum + array[i, j];
+    }
+    Console.WriteLine($"Среднее арифметическое столбца: {sum / array.GetLength(0)}");
+}
+
+
+
